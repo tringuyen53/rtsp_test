0750f7a8b9720392b9a17564703972b830f27c5d
tringuyen53 <nguyenduongtri0503@gmail.com>
2021-12-09 17:25:16 +0700

    remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,245 +1,3 @@
-// use opencv::{Result, imgcodecs, prelude::*, videoio};
-// fn main() -> Result<()> {
-//     println!("Hello, world!");
-//     let mut cam = videoio::VideoCapture::from_file("rtsp://10.50.31.241/axis-media/media.amp",
-//      videoio::CAP_GSTREAMER).unwrap(); // 0 is the default camera
-//     let opened = videoio::VideoCapture::is_opened(&cam).unwrap();
-// 	if !opened {
-// 		panic!("Unable to open default camera!");
-// 	}
-//     let mut img_count = 0;
-//     let mut count = 0;
-//     let mut params = opencv::types::VectorOfi32::new();
-//     let mut frame_buffer = opencv::types::VectorOfu8::new();
-//     params.push(imgcodecs::IMWRITE_JPEG_QUALITY);
-//     params.push(50);
-//     loop {
-// 		let mut frame = Mat::default();
-// 		let is_read = cam.read(&mut frame)?;
-//         if is_read {
-//             //Save frame to jpg image.
-//                 // imgcodecs::imwrite(format!("img-{}.jpg",img_count).as_str(), &frame, &params)?;
-//                 imgcodecs::imencode(".jpg", &frame, &mut frame_buffer, &params)?;
-//                 println!("frame buffer length: {}", frame_buffer.len());
-//             //     let img_result =
-//             //     image::load_from_memory_with_format(frame_buffer.as_slice(), image::ImageFormat::Jpeg);
-//             // let img = match img_result {
-//             //     Ok(image) => image,
-//             //     Err(_) => {
-//             //         println!("not image format");
-//             //         return Ok(())
-//             //     },
-//             // };
-//             // img.save(format!("img_bytes-{}.jpg", img_count)).unwrap();
-//             // let img16 = img.into_rgb8();
-//             // let data = img16.into_raw() as Vec<u8>;
-//             // println!("Image length: {}", data.len());
-//             count += 1;
-//             count += 15;
-//             let val = cam.get(videoio::CAP_PROP_POS_FRAMES)?;
-//             println!("Get: {}", val);
-//             cam.set(videoio::CAP_PROP_POS_FRAMES, count as f64)?;
-//         } else {
-//             cam.release();
-//             break;
-//         }
-//         println!("End of 1 frame.");
-//         img_count += 1;
-// 		// let key = highgui::wait_key(10)?;
-// 		// if key > 0 && key != 255 {
-// 		// 	break;
-// 		// }
-// 	}
-// 	Ok(())
-// }
-
-//FFMPEG-RUST
-// extern crate ffmpeg_next as ffmpeg;
-
-// use ffmpeg::format::{input, Pixel};
-// use ffmpeg::media::Type;
-// use ffmpeg::software::scaling::{context::Context, flag::Flags};
-// use ffmpeg::util::frame::video::Video;
-// use std::fs::File;
-// use std::io::prelude::*;
-// use std::thread;
-// use tokio::runtime::Handle;
-
-// #[tokio::main]
-// async fn main() {
-//     let handle = Handle::current();
-
-//     let urls = [
-//         // "rtsp://vietnam:L3xRay123!@10.50.30.212/1/h264major",
-//         // "rtsp://10.50.29.36/1/h264major",
-//         // "rtsp://10.50.31.171/1/h264major",
-//         // "rtsp://vietnam:L3xRay123!@10.50.12.187/media/video1",
-//         "rtsp://10.50.30.100/1/h264major",
-
-//     ];
-
-//     for url in urls {
-//         handle.spawn(async move { get_frame(url).await });
-//     }
-
-//     loop {}
-// }
-
-// async fn get_frame(cam_url: &str) -> Result<(), ffmpeg::Error> {
-//     ffmpeg::init().unwrap();
-
-//     println!("{:?}", cam_url);
-
-//     let path = cam_url.clone();
-//     if let Ok(mut ictx) = input(&path) {
-//         let input = ictx
-//             .streams()
-//             .best(Type::Video)
-//             .ok_or(ffmpeg::Error::StreamNotFound)?;
-//         let video_stream_index = input.index();
-
-//         let mut decoder = input.codec().decoder().video()?;
-//         //  println!("Codec: {:?}", input.codec());
-
-//         let mut scaler = Context::get(
-//             decoder.format(),
-//             decoder.width(),
-//             decoder.height(),
-//             Pixel::RGB24,
-//             decoder.width(),
-//             decoder.height(),
-//             Flags::BILINEAR,
-//         )?;
-
-//         let mut frame_index = 0;
-
-//         let mut receive_and_process_decoded_frames =
-//             |decoder: &mut ffmpeg::decoder::Video| -> Result<(), ffmpeg::Error> {
-//                 let mut decoded = Video::empty();
-//                 while decoder.receive_frame(&mut decoded).is_ok() {
-//                     let mut rgb_frame = Video::empty();
-//                     scaler.run(&decoded, &mut rgb_frame)?;
-//                     // save_file(&rgb_frame, frame_index).unwrap();
-//                     frame_index += 1;
-//                 }
-//                 Ok(())
-//             };
-
-//         for (stream, packet) in ictx.packets() {
-//             if stream.index() == video_stream_index {
-//                 decoder.send_packet(&packet)?;
-//                 receive_and_process_decoded_frames(&mut decoder)?;
-//             }
-//         }
-//         decoder.send_eof()?;
-//         receive_and_process_decoded_frames(&mut decoder)?;
-//     }
-//     // loop{}
-//     Ok(())
-// }
-
-// fn save_file(frame: &Video, index: usize) -> std::result::Result<(), std::io::Error> {
-//     let mut file = File::create(format!("frame{}.ppm", index))?;
-//     file.write_all(format!("P6\n{} {}\n255\n", frame.width(), frame.height()).as_bytes())?;
-//     // file.write_all(frame.data(0))?;
-//     let data = frame.data(0);
-//     let stride = frame.stride(0);
-//     let byte_width: usize = 3 * frame.width() as usize;
-//     let height: usize = frame.height() as usize;
-//     for line in 0..height {
-//         let begin = line * stride;
-//         let end = begin + byte_width;
-//         file.write_all(&data[begin..end])?;
-//     }
-//     Ok(())
-// }
-
-//FFAV
-// use clap::{App, Arg};
-// use ffav::easy::{AVError, AVFrameOwned, SimpleDecoder, SimpleReader};
-// use ffav::ffi::{AVCodecID, AVPacket};
-// use std::convert::TryInto;
-// use std::sync::{
-//     atomic::{AtomicBool, Ordering},
-//     Arc,
-// };
-// use std::time::Instant;
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // let matches = App::new("Simple Decoder")
-//     //     .version("1.0")
-//     //     .author("Varphone Wong <varphone@qq.com>")
-//     //     .about("Example for SimpeDecoder")
-//     //     .arg(
-//     //         Arg::with_name("FILE")
-//     //             .help("Sets the input file to use for decoding")
-//     //             .required(true),
-//     //     )
-//     //     .get_matches();
-
-//     // let input_file = matches
-//     //     .value_of("FILE")
-//     //     .ok_or("The input file must be specified")?;
-//     let input_file = "rtsp://10.50.30.100/1/h264major";
-
-//     let early_exit = Arc::new(AtomicBool::new(false));
-//     let early_exit_cloned = Arc::clone(&early_exit);
-//     let early_exit_thread = std::thread::spawn(move || {
-//         let mut buffer = String::new();
-//         std::io::stdin().read_line(&mut buffer).unwrap();
-//         early_exit_cloned.store(true, Ordering::SeqCst);
-//     });
-
-//     ffav::util::error::register_all();
-
-//     let mut decoder = SimpleDecoder::new(AVCodecID::AV_CODEC_ID_H264)?;
-//     let mut reader = SimpleReader::open(input_file, None, None)?;
-//     let stream_codecs: Vec<AVCodecID> = reader
-//         .streams()
-//         .iter()
-//         .map(|x| match x.codecpar() {
-//             Some(v) => v.codec_id,
-//             None => AVCodecID::AV_CODEC_ID_NONE,
-//         })
-//         .collect();
-
-//     println!("streams()={:#?}", reader.streams());
-//     for s in reader.streams() {
-//         println!("codecpar={:#?}", s.codecpar().unwrap());
-//     }
-
-//     let mut frame = AVFrameOwned::new()?;
-
-//     for (mut packet, _info) in reader.frames() {
-//         if early_exit.load(Ordering::SeqCst) {
-//             break;
-//         }
-
-//         let bytes =
-//             unsafe { std::slice::from_raw_parts(packet.data, packet.size.try_into().unwrap()) };
-//         let n = bytes.len().min(16);
-//         if stream_codecs[packet.stream_index as usize] == AVCodecID::AV_CODEC_ID_H264 {
-//             match decoder.receive_frame(&mut frame) {
-//                 Ok(_) => {
-//                     // println!("Frame {:#?}", frame)
-
-//             },
-//                 Err(e) => match e {
-//                     AVError::Again => { // Try next
-//                     }
-//                     e => println!("Error: {:?}", e),
-//                 },
-//             }
-//             let r = decoder.send_packet(&mut packet);
-//             // println!("r2={:?}", r);
-//         }
-//     }
-
-//     early_exit_thread.join().unwrap();
-
-//     Ok(())
-// }
-
 //GSTREAMER
 
 // This example demonstrates how to get a raw video frame at a given position
@@ -280,7 +38,7 @@ struct ErrorMessage {
     source: glib::Error,
 }
 
-fn create_pipeline(uri: String, out_path: std::path::PathBuf) -> Result<gst::Pipeline, Error> {
+fn create_pipeline(uri: String) -> Result<gst::Pipeline, Error> {
     gst::init()?;
 
     // Create our pipeline from a pipeline description string.
@@ -292,6 +50,10 @@ fn create_pipeline(uri: String, out_path: std::path::PathBuf) -> Result<gst::Pip
         "rtspsrc location={} latency=0 ! queue ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! avdec_h264 ! queue ! video/x-raw ! jpegenc ! image/jpeg ! appsink name=sink" ,
         uri
     ))?
+    // let pipeline = gst::parse_launch(&format!(
+    //     "rtspsrc location={} latency=0 ! queue ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! vaapih264dec ! queue ! video/x-raw ! jpegenc ! image/jpeg ! appsink name=sink" ,
+    //     uri
+    // ))?
     // let pipeline = gst::parse_launch(&format!(
     //     "rtspsrc location={} latency=0 ! queue ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! vaapih263dec ! queue ! videoconvert ! videoscale ! jpegenc ! appsink name=sink" ,
     //     uri
@@ -390,104 +152,6 @@ fn create_pipeline(uri: String, out_path: std::path::PathBuf) -> Result<gst::Pip
                 // let data = img16.into_raw() as Vec<u8>;
                 // println!("Image length: {}", data.len());
                 
-
-
-
-                // Make sure that we only get a single buffer
-                // if got_snapshot {
-                //     return Err(gst::FlowError::Eos);
-                // }
-                // got_snapshot = true;
-
-                // let caps = sample.caps().expect("Sample without caps");
-                // let info = gst_video::VideoInfo::from_caps(caps).expect("Failed to parse caps");
-                // println!("info: {:?}", info);
-            //     // // At this point, buffer is only a reference to an existing memory region somewhere.
-            //     // // When we want to access its content, we have to map it while requesting the required
-            //     // // mode of access (read, read/write).
-            //     // // This type of abstraction is necessary, because the buffer in question might not be
-            //     // // on the machine's main memory itself, but rather in the GPU's memory.
-            //     // // So mapping the buffer makes the underlying memory region accessible to us.
-            //     // // See: https://gstreamer.freedesktop.org/documentation/plugin-development/advanced/allocation.html
-                // let frame = gst_video::VideoFrameRef::from_buffer_ref_readable(buffer, &info)
-                //     .map_err(|_| {
-                //         element_error!(
-                //             appsink,
-                //             gst::ResourceError::Failed,
-                //             ("Failed to map buffer readable")
-                //         );
-
-                //         gst::FlowError::Error
-                //     })?;
-
-                // // We only want to have a single buffer and then have the pipeline terminate
-                // println!("Have video frame: {:?}", frame.buffer());
-                
-
-            //     // Calculate a target width/height that keeps the display aspect ratio while having
-            //     // a height of 240 pixels
-            //     // let display_aspect_ratio = (frame.width() as f64 * *info.par().numer() as f64)
-            //     //     / (frame.height() as f64 * *info.par().denom() as f64);
-            //     // let target_height = 240;
-            //     // let target_width = target_height as f64 * display_aspect_ratio;
-
-                // Create a FlatSamples around the borrowed video frame data from GStreamer with
-                // the correct stride as provided by GStreamer.
-                // let img = image::FlatSamples::<&[u8]> {
-                //     samples: frame.plane_data(0).unwrap(),
-                //     layout: image::flat::SampleLayout {
-                //         channels: 3,       // RGB
-                //         channel_stride: 1, // 1 byte from component to component
-                //         width: frame.width(),
-                //         width_stride: 4, // 4 byte from pixel to pixel
-                //         height: frame.height(),
-                //         height_stride: frame.plane_stride()[0] as usize, // stride from line to line
-                //     },
-                //     color_hint: Some(image::ColorType::Rgb8),
-                // };
-                
-
-            //     //SAVING IMAGE
-            //     let img = 
-            //     image::load_from_memory_with_format(&frame.plane_data(0).unwrap(), ImageFormat::Jpeg).unwrap();
-            // // let img = match img_result {
-            // //     Ok(image) => image,
-            // //     Err(_) => ,
-            // // };
-            //     img.save(format!("img-{}", count)).unwrap();
-            //     let img16 = img.into_rgb8();
-            //     let data = img16.into_raw() as Vec<u8>;
-            //     println!("Image length: {}", data.len());
-            //     count += 1;
-
-                // // let img_buffer = img.as_slice();                
-
-                // // // Scale image to our target dimensions
-                // let scaled_img = image::imageops::thumbnail(
-                //     &img.as_view::<image::Rgb<u8>>()
-                //         .expect("couldn't create image view"),
-                //         target_width as u32,
-                //     target_height as u32,
-                // );
-
-                // // // Save it at the specific location. This automatically detects the file type
-                // // // based on the filename.
-                // scaled_img.save(&out_path).map_err(|err| {
-                //     element_error!(
-                //         appsink,
-                //         gst::ResourceError::Write,
-                //         (
-                //             "Failed to write thumbnail file {}: {}",
-                //             out_path.display(),
-                //             err
-                //         )
-                //     );
-
-                //     gst::FlowError::Error
-                // })?;
-
-                // println!("Wrote thumbnail to {}", out_path.display());
-
                 Ok(gst::FlowSuccess::Ok)
                 // Err(gst::FlowError::Error)
             })
@@ -497,7 +161,7 @@ fn create_pipeline(uri: String, out_path: std::path::PathBuf) -> Result<gst::Pip
     Ok(pipeline)
 }
 
-fn main_loop(pipeline: gst::Pipeline, position: u64) -> Result<(), Error> {
+fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
     println!("Start main loop");
     pipeline.set_state(gst::State::Playing)?;
 
@@ -554,12 +218,12 @@ fn main() {
     // let out_path = args
     //     .next()
     //     .expect("No output path provided on the commandline");
-    let uri = "rtsp://10.50.30.100/1/h264major".to_owned();
-    let position = 10;
-    let out_path = "/img";
-    let out_path = std::path::PathBuf::from(out_path);
+    // let uri = "rtsp://10.50.13.231/1/h264major".to_owned();
+    // let position = 10;
+    // let out_path = "/img";
+    // let out_path = std::path::PathBuf::from(out_path);
 
-    match create_pipeline(uri, out_path).and_then(|pipeline| main_loop(pipeline, position)) {
+    match create_pipeline(uri).and_then(|pipeline| main_loop(pipeline)) {
         Ok(r) => r,
         Err(e) => eprintln!("Error! {}", e),
     }
