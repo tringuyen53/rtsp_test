6e9ffa97bf4bb6a240c8863523feed504d3c4c31
trvinh99 <48145607+trvinh99@users.noreply.github.com>
2022-01-25 15:37:21 +0700

    push

--- src/main.rs
+++ src/main.rs
@@ -168,7 +168,9 @@ fn create_pipeline(uri: String, seed: u8) -> Result<gst::Pipeline, Error> {
 
                 let mut samples = samples.clone();
 
-                let packet = Packet::unmarshal(&mut samples).unwrap();
+                let pkt = Packet::default();
+
+                let packet = pkt.unmarshal(&mut samples).unwrap();
 
                 h264writer.write_rtp(&packet).unwrap();
                 h264writer.close().unwrap();
