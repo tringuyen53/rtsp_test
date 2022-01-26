6fc925f8ceeb04dda9cd617bd4d5cc5daa5d50f0
trvinh99 <48145607+trvinh99@users.noreply.github.com>
2022-01-26 16:44:59 +0700

    push

--- src/main.rs
+++ src/main.rs
@@ -193,6 +193,8 @@ fn create_pipeline(uri: String, seed: u8) -> Result<gst::Pipeline, Error> {
                     .unwrap();
 
                     i = i + 1;
+
+                    count = 0;
                     println!("Count {} int Sucess time: {:?}", i, naive);
 
                     // println!("NEXT INDEX FRAME: {:?}", now - time);
