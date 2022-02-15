defb4796898175f1d1122818ec913c87ca37f3f3
tringuyen53 <nguyenduongtri0503@gmail.com>
2022-02-15 14:54:54 +0700

    test framerate using spawn

--- src/main.rs
+++ src/main.rs
@@ -381,13 +381,13 @@ async fn main() {
 
     let urls = [
         // "rtsp://10.50.29.36/1/h264major",
-        "rtsp://10.50.31.171/1/h264major",
+        // "rtsp://10.50.31.171/1/h264major",
         // "rtsp://10.50.31.172/1/h264major",
-        "rtsp://10.50.13.231/1/h264major",
-        "rtsp://10.50.13.233/1/h264major",
+        // "rtsp://10.50.13.231/1/h264major",
+        // "rtsp://10.50.13.233/1/h264major",
         // "rtsp://10.50.13.234/1/h264major",
         // "rtsp://10.50.13.235/1/h264major",
-        "rtsp://10.50.13.236/1/h264major",
+        // "rtsp://10.50.13.236/1/h264major",
         "rtsp://10.50.13.237/1/h264major",
         // "rtsp://10.50.13.238/1/h264major",
         // "rtsp://10.50.13.239/1/h264major",
@@ -410,13 +410,13 @@ async fn main() {
 
     let cam_ip = vec![
         // 36,
-        171,
+        // 171,
         // 172, 
-        231, 
-        233, 
+        // 231, 
+        // 233, 
         // 234, 
         // 235, 
-        236, 
+        // 236, 
         237, 
         // 238, 
         // 239, 
@@ -493,7 +493,7 @@ async fn get_rtsp_stream(ctx: BastionContext) -> Result<(), ()> {
 //let mut rng = rand::thread_rng();                
 //let n1: u8 = rng.gen();
 println!("spawn new actor: {:?}", message.id);
-let rt = tokio::runtime::Runtime::new().unwrap();
+// let rt = tokio::runtime::Runtime::new().unwrap();
     let is_frame_getting = is_frame_getting.clone();
     let is_record = is_record.clone();
     let is_live = is_live.clone();
