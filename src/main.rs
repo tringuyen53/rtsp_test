c7af494e12ff37c8f1dfab0e1e5db5e1b193eda3
tringuyen53 <nguyenduongtri0503@gmail.com>
2022-02-17 16:13:41 +0700

    test is getting frame stop

--- src/main.rs
+++ src/main.rs
@@ -214,6 +214,7 @@ async fn connect_nats() -> Connection {
                             });
                         }
                     }
+                    return Err(gst::FlowError::Eos);
                 }      
 
         //        println!("Buffer {:?}", buffer);
