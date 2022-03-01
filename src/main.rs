28e4460306adea6862236ae0e833b06fe0b35aa6
tringuyen53 <nguyenduongtri0503@gmail.com>
2022-03-01 11:50:11 +0700

    test 31.171

--- src/main.rs
+++ src/main.rs
@@ -187,7 +187,7 @@ async fn connect_nats() -> Connection {
              match img_result {
                  Ok(image) => {
                         //  image.save(format!("full-{}-{}.jpg", id, count_full)).unwrap();
-                        image.save(format!("{}.jpg", std::time::SystemTime::now()));
+                        image.save(format!("{:?}.jpg", std::time::SystemTime::now()));
                          count_full += 1;
                     },
                  Err(_) => (),
